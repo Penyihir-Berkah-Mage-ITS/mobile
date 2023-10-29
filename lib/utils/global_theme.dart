884c2a6953658af71ea2fd3de8bffb0643ec1f9c
mfadlihs <winnerfadli999@gmail.com>
2023-10-29 11:54:36 +0700

    color constants and text

---
+++ lib/utils/global_theme.dart
@@ -0,0 +1,10 @@
+import 'package:asa/styles/color_constants.dart';
+import 'package:flutter/material.dart';
+
+ThemeData globalTheme() {
+  return ThemeData(
+    fontFamily: "Hauora",
+    scaffoldBackgroundColor: Colors.white,
+    colorScheme: appColorScheme,
+  );
+}
